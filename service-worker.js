660eab4a5c525a563c7b06dd3428236f06e65ad6
Milad305 <58796025+Milad305@users.noreply.github.com>
2024-04-18 23:35:55 +0330

    firsti

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+// Define a cache name
+const CACHE_NAME = 'tesso-cache-v1';
+
+// Specify the files to cache
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js',
+  // Add other URLs to cache
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
