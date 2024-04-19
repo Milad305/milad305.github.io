d48e69576b017c457232f941f1d30df79d97d80b
Milad305 <milad18mw@gmail.com>
2024-04-19 23:31:36 +0330

    s

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "314ddb24373de1f1775a403f597acc61
 "icons/Icon-maskable-1024.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b76e5f2aff95f6c49e6393b6baab1a7b",
-"/": "b76e5f2aff95f6c49e6393b6baab1a7b",
-"main.dart.js": "4356e69a9c30de14e1480f296bb74fcf",
+"index.html": "7c6766e30b0b8657f7d0aee59d4db3d8",
+"/": "7c6766e30b0b8657f7d0aee59d4db3d8",
+"main.dart.js": "660c06b0978d6d8766881df1662a0c6d",
 "manifest.json": "c9fa30d13cb9e9f8c2150188357c3f1f",
 "service-worker.js": "b89168fc28548877ae87ac15cd9bf19e",
 "version.json": "74eeb008593826c0b4bd1c870326c17c"};
